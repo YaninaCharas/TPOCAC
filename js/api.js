ee91cac8412aab8a8ba0f7c925b614f3c445308a
Yanina Charas <ycharas@gmail.com>
2022-10-26 22:16:31 -0300

    agregue API Provincia

---
+++ js/api.js
@@ -0,0 +1,26 @@
+const fetchApp = (provincia) =>{
+    return fetch(`https://apis.datos.gob.ar/georef/api/provincias?nombre=${provincia}`)
+    .then ( response => response.json())
+    .catch ( error => console.log(error))
+}
+
+const $provincia = document.getElementById(`provincia`);
+const $form = document.querySelector(`form`);
+const $submit = document.querySelector(`#submit`);
+
+$form.addEventListener(`submit`, async function(event)
+{
+    event.preventDefault();
+    const valorProvincia = $provincia.value;
+
+
+if (!valorProvincia) return 
+
+let dateFetchApp = await fetchApp(valorProvincia);
+
+if (dateFetchApp){
+    console.log(dateFetchApp)
+    alert(dateFetchApp)
+}
+
+})
